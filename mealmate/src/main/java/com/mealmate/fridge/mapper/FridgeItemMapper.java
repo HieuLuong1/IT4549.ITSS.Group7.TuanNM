a95b6cbfd11dd8d11d138b1d8cdded5134a2e8ae
nqocdungg <ngocdungcbnn@gmail.com>
2026-06-16 00:25:40 +0700

    feat(fridge): enable editing food items in popup

--- mealmate/src/main/java/com/mealmate/fridge/mapper/FridgeItemMapper.java
+++ mealmate/src/main/java/com/mealmate/fridge/mapper/FridgeItemMapper.java
@@ -35,6 +35,7 @@ public class FridgeItemMapper {
         response.setId(item.getId());
         response.setFamilyId(item.getFamilyId());
         response.setFoodId(item.getFoodId());
+        response.setCustomName(item.getCustomName());
         response.setDisplayName(item.getCustomName());
         response.setPreservationMethods(List.of());
         response.setQuantity(item.getQuantity());
@@ -62,6 +63,7 @@ public class FridgeItemMapper {
         response.setFamilyId(projection.getFamilyId());
         response.setFoodId(projection.getFoodId());
         response.setStandardFoodName(projection.getStandardFoodName());
+        response.setCustomName(projection.getCustomName());
         response.setDisplayName(projection.getDisplayName());
         response.setUnit(projection.getUnit());
         response.setCategoryId(projection.getCategoryId());
